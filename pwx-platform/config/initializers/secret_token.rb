b04af9d77f24d41fad73f1f5c11d31684c5cbdfa
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2014-07-27 08:30:58 -0700

    Initial application for PWX Platform

---
+++ pwx-platform/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PwxPlatform::Application.config.secret_token = '5280873f57d7928cbc438f5acfdfcef355c2550083a6e55fd3b4624e67a2aa01c4eab3841a26cf685de3a51509967663a25f458a14cb987dce92e0bfbf161efe'
